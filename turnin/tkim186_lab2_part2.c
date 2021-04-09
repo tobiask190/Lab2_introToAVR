5f31a00840f25c7bc7ebb9562d4d632ce6991394
tobiask190 <44378884+tobiask190@users.noreply.github.com>
2021-04-08 18:58:24 -0700

    Update tkim186_lab2_part2.c

--- turnin/tkim186_lab2_part2.c
+++ turnin/tkim186_lab2_part2.c
@@ -13,24 +13,30 @@
 #endif
 
 int main(void) {
-    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-    DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-    unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
-    unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+    	
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+    	unsigned char tmpC = 0x00; // Temporary variable to hold the value of B
+    	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+	unsigned char i = 0;
+	unsigned char count = 0;
+	
 	while(1) {
-        // 1) Read input
-        tmpA = PINA & 0x01;
-        // 2) Perform computation
-        // if PA0 is 1, set PB1PB0 = 01, else = 10
-        if (tmpA == 0x01) { // True if PA0 is 1
-            tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
-                             // (clear rightmost 2 bits, then set to 01)
-        } else {
-            tmpB = (tmpB & 0xFC) | 0x02; // Sets tmpB to bbbbbb10
-                             // (clear rightmost 2 bits, then set to 10)
-        }    
-    // 3) Write output
-		PORTB = tmpB;    
+		tmpA = PINA & 0x0F;
+		count = 0;
+		for (i = 0; i < 4; i++) {
+			if ( (tmpA >> i) & 0x01 ) {
+				count++;	
+			}
+		}
+		PORTC = count;
+		//tmpA = PINA & 0x01;
+		//if (tmpA == 0x01) { // True if PA0 is 1
+		  //  tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
+		//} else {
+		  //  tmpB = (tmpB & 0xFC) | 0x02; // Sets tmpB to bbbbbb10
+        	//}    
+		///PORTB = tmpB;    
     }
     return 0;
 }
